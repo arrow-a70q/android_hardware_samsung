79b7225f83114c86aa5752be72465cf77facbe91
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-11-24 04:52:07 +0500

    Drop already defined module (ril)

--- Android.mk
+++ Android.mk
@@ -39,5 +39,4 @@ ifeq ($(BOARD_VENDOR),samsung)
 include $(SAM_ROOT)/audio/Android.mk
 include $(SAM_ROOT)/hidl/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
-include $(SAM_ROOT)/ril/Android.mk
 endif
